4272f56edc895dca3d4e60138b3d8387c7ef56b1
kosuri-hp <kosuribhargavi1836@gmail.com>
2025-02-12 16:41:59 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,90 @@
+//1 
+Function sayHello(){
+    console.log("Hello,world!")
+}
+sayHello()
+
+// 2
+function displaywarning(){
+    console.log("warning!system overload!")
+}
+displaywarning
+
+// 3
+function printstatus(){
+    console.log("system status:active")
+}
+printstatus()
+
+// 4
+function intialize(){
+    console.log("initialization complete.")
+    shutdown()
+}
+Function intialize(){
+    console.log(shutdown function)
+}
+intialize()
+
+// 5
+function start process(){
+    console.log("process started")
+
+}
+end process()
+function end process(){
+    console.log("process EncodedAudioChunk.")
+
+}
+startprocess()
+
+// 6
+function greetuser(){
+    console.log("Hello,name")
+
+}
+greetuser("Bhargavi")
+
+// 7
+functioncalculate SubmitEvent(){
+    console.log("a+b")
+    }
+    console(2+3)
+    console(5+2)
+
+    // 8
+    
+    var n=5
+    function countDown(){
+    
+      for (var i=5;i>=1;i--){
+    console.log(i)
+    
+    }
+    }
+    countDown()
+
+    // 9
+    function checkEvenOdd(){
+        var p=8
+        if(p%2==0){
+         console.log("even")
+         }
+       else{
+       console.log("odd")        
+       } 
+       console.log(p) 
+     }
+     checkEvenOdd()
+     
+     
+     //10
+     
+     function beginSession(){
+     console.log("Session Started.")
+     endSession();
+     }
+     function endSession(){
+     console.log("Session Ended.")
+     }
+     beginSession()
